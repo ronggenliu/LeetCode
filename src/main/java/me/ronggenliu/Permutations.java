3423c13b6d407b7a5ad4b1b1360d6e0e350726a4
ronggenliu <ronggenliu@gmail.com>
2018-11-17 20:51:15 +0800

    Permutations

---
+++ src/main/java/me/ronggenliu/Permutations.java
@@ -0,0 +1,12 @@
+package me.ronggenliu;
+
+import java.util.List;
+
+/**
+ * Given a collection of distinct integers, return all possible permutations.
+ */
+public class Permutations {
+    public List<List<Integer>> permute(int[] nums) {
+
+    }
+}

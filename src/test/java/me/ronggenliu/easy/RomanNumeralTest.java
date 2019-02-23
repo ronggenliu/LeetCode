b2287fab7bab6332090aa9f1e4190b4f6750e777
ronggenliu <ronggenliu@gmail.com>
2019-02-23 21:39:19 +0800

    clean up code.

--- src/test/java/me/ronggenliu/easy/RomanNumeralTest.java
+++ src/test/java/me/ronggenliu/easy/RomanNumeralTest.java
@@ -3,8 +3,6 @@ package me.ronggenliu.easy;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 

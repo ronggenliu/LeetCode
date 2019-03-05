fc650a093cbea34c476ea8ea31700b5b334ab816
ronggenliu <ronggenliu@gmail.com>
2019-03-05 10:06:12 +0800

    use parameterized UT.

--- src/test/java/me/ronggenliu/easy/LongestCommonPrefixTest.java
+++ src/test/java/me/ronggenliu/easy/LongestCommonPrefixTest.java
@@ -3,35 +3,25 @@ package me.ronggenliu.easy;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
+import org.junit.runners.Parameterized.Parameter;
 
 import static org.junit.Assert.assertEquals;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-
-import org.junit.Before;
 
 /**
  * Unit test for simple PalindromeNumber.
  */
 @RunWith(Parameterized.class)
  public class LongestCommonPrefixTest {
+  @Parameter
   public LongestCommonPrefix longestCommonPrefix;
 
-  public LongestCommonPrefixTest(LongestCommonPrefix lCommonPrefix) {
-    this.longestCommonPrefix = lCommonPrefix;
-  }
-
   @Parameters
-  public static Collection<Object[]> classes() {
-    return Arrays.asList(new Object[][] {
-      { new LongestCommonPrefixImpl1() },
-      { new LongestCommonPrefixImpl2() }
-    });
+  public static Object[] data() {
+    return new Object[] {
+      new LongestCommonPrefixImpl1(),
+      new LongestCommonPrefixImpl2()
+    };
   }
 
   @Test

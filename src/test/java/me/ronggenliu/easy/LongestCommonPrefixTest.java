faac022979b7d3c7d1d3e9bb39ce56cac1075e03
ronggenliu <ronggenliu@gmail.com>
2019-03-05 10:53:18 +0800

    refine parameterized UT.

--- src/test/java/me/ronggenliu/easy/LongestCommonPrefixTest.java
+++ src/test/java/me/ronggenliu/easy/LongestCommonPrefixTest.java
@@ -8,27 +8,33 @@ import org.junit.runners.Parameterized.Parameter;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 /**
  * Unit test for simple PalindromeNumber.
  */
 @RunWith(Parameterized.class)
- public class LongestCommonPrefixTest {
+public class LongestCommonPrefixTest {
   @Parameter
   public LongestCommonPrefix longestCommonPrefix;
 
-  @Parameters
-  public static Object[] data() {
-    return new Object[] {
-      new LongestCommonPrefixImpl1(),
-      new LongestCommonPrefixImpl2()
-    };
+  @Parameter(1)
+  public String clazz;
+
+  @Parameters(name = "{index}: test for impl {1}")
+  public static Collection<Object[]> data() {
+    return Arrays.asList(new Object[][]{
+        {new LongestCommonPrefixImpl1(), "LongestCommonPrefixImpl1"},
+        {new LongestCommonPrefixImpl2(), "LongestCommonPrefixImpl2"}
+    });
   }
 
   @Test
   public void testResultWithResult() {
-    assertEquals("fl", longestCommonPrefix.longestCommonPrefix(new String[]{"flower","flow","flight"}));
-    assertEquals("", longestCommonPrefix.longestCommonPrefix(new String[]{"dog","racecar","car"}));
-    assertEquals("", longestCommonPrefix.longestCommonPrefix(new String[]{"c","ac","cc"}));
+    assertEquals("fl", longestCommonPrefix.longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+    assertEquals("", longestCommonPrefix.longestCommonPrefix(new String[]{"dog", "racecar", "car"}));
+    assertEquals("", longestCommonPrefix.longestCommonPrefix(new String[]{"c", "ac", "cc"}));
     assertEquals("dog", longestCommonPrefix.longestCommonPrefix(new String[]{"dog"}));
     assertEquals("", longestCommonPrefix.longestCommonPrefix(new String[]{}));
   }
